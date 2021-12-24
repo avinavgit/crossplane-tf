0a424457999e5a666f4017d587ebd9fe5cb6afc0
avinavgit <44772156+avinavgit@users.noreply.github.com>
2021-12-24 19:41:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+region = var.location
+   shared_credentials_file = "aws-credentials"
+   profile                 = "default"
+}
+resource "aws_security_group" "allow_tls" {
+name        = "allow_tls"
+description = "Allow TLS inbound traffic"
+vpc_id      = "vpc-a5ca90c0"
+
+ingress {
+  description      = "TLS from VPC"
+  from_port        = 443
+  to_port          = 443
+  protocol         = "tcp"
+  cidr_blocks      = ["10.0.0.0/8"]
+
+}
+
+egress {
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+
+}
+
+tags = {
+  Name = "allow_tls_crossplane"
+}
+}
+
+variable location {
+}
